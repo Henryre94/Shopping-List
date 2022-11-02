0ab851326723b8fa625431a1933be04e039563e1
David Schwarz <david@ists-wurscht.at>
2022-11-03 00:38:13 +0100

    Project structure started

---
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/BestellplattformApplication.java
@@ -0,0 +1,13 @@
+package at.aschowurscht.dev.saadi.erp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestellplattformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestellplattformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/at/aschowurscht/dev/saadi/erp/backend/BestellplattformApplicationTests.java
@@ -0,0 +1,13 @@
+package at.aschowurscht.dev.saadi.erp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestellplattformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
