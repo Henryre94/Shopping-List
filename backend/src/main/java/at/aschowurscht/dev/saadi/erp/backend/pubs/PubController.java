b96435da37d6a1d8b4cc35b762956f9ce1296bf4
Henry <henry.rauchwerger@outlook.com>
2022-11-11 13:32:58 +0100

    Anotations, Autowired und Variablen zu Pub hinzugefügt

--- backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/pubs/PubController.java
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/pubs/PubController.java
@@ -1,4 +1,10 @@
 package at.aschowurscht.dev.saadi.erp.backend.pubs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PubController {
+    @Autowired
+    PubService pubService;
 }
--- backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/pubs/PubService.java
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/pubs/PubService.java
@@ -1,4 +1,10 @@
 package at.aschowurscht.dev.saadi.erp.backend.pubs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PubService {
+    @Autowired
+    PubCRUDRepository pubCRUDRepository;
 }
