926807e42a488a0e1e494ed15a52fd15f9b1c3e3
Ahmed El Seifi <ahmed.el-seifi@hotmail.com>
2022-11-16 14:01:55 +0100

    AdminView erstellt

---
+++ frontend/src/views/AdminView.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container>
+        <h1>Verwaltungstabelle</h1>
+        <AdminTable/>
+    </v-container>
+</template>
+
+<script>
+
+
+import AdminTable from "@/components/AdminTable";
+
+export  default {
+    name: 'AdminView.vue',
+    components :{
+        AdminTable,
+    }
+}
+</script>
