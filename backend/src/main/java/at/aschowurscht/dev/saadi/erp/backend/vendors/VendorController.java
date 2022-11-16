59bbd25c203ddb2e1cc73913f30fb245aa15795b
Henry <henry.rauchwerger@outlook.com>
2022-11-16 14:07:21 +0100

    Vendor CRUD-Methoden implementiert

--- backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/vendors/VendorController.java
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/vendors/VendorController.java
@@ -1,10 +1,37 @@
 package at.aschowurscht.dev.saadi.erp.backend.vendors;
 
+import at.aschowurscht.dev.saadi.erp.backend.pubs.Pub;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class VendorController {
     @Autowired
     VendorService vendorService;
+    @CrossOrigin
+    @PostMapping("/api/vendors")
+    public void postProduct(@RequestBody Vendor vendor){
+        vendorService.post(vendor);
+    }
+    @CrossOrigin
+    @GetMapping("/api/vendors/{venId}")
+    public Vendor getById(@PathVariable int venId){
+        return vendorService.getById(venId);
+    }
+    @CrossOrigin
+    @GetMapping("/api/vendors")
+    public List<Vendor> get(){
+        List<Vendor> pubList = vendorService.get();
+        return pubList;
+    }
+    @CrossOrigin
+    @PutMapping("/api/vendors/{pubId}")
+    public void put(@PathVariable int pubId){
+        vendorService.put(pubId);
+    }
+    @CrossOrigin
+    @DeleteMapping("/vendors/{venId}")
+    public void delete(@PathVariable int venId) {vendorService.delete(venId);}
 }
--- backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/vendors/VendorService.java
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/vendors/VendorService.java
@@ -1,10 +1,31 @@
 package at.aschowurscht.dev.saadi.erp.backend.vendors;
 
+import at.aschowurscht.dev.saadi.erp.backend.products.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VendorService {
     @Autowired
     VendorCRUDRepository vendorCRUDRepository;
+
+
+    public void post(Vendor vendor){
+        vendorCRUDRepository.save(vendor);
+    }
+    public Vendor getById(int venId){
+        Vendor vendor = vendorCRUDRepository.findById(venId).get();
+        return vendor;
+    }
+    public List<Vendor> get(){
+        return ((List<Vendor>) vendorCRUDRepository.findAll());
+    }
+    public void put(int venId){
+        vendorCRUDRepository.findById(venId);
+    }
+    public void delete(int venId){
+        vendorCRUDRepository.deleteById(venId);
+    }
 }
