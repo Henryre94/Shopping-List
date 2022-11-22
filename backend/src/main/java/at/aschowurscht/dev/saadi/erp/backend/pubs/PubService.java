5f8b826ce074b7c9ce5e4fbc8d3bf2905bc6f92a
Henry <henry.rauchwerger@outlook.com>
2022-11-22 13:03:06 +0100

    ResponseStatusException hinzugefügt

--- backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/pubs/PubService.java
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/pubs/PubService.java
@@ -1,9 +1,12 @@
 package at.aschowurscht.dev.saadi.erp.backend.pubs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PubService {
@@ -13,8 +16,11 @@ public class PubService {
         pubCRUDRepository.save(pub);
     }
     public Pub getById(int pubId){
-        Pub pub = pubCRUDRepository.findById(pubId).get();
-        return pub;
+        Optional<Pub> pub = pubCRUDRepository.findById(pubId);
+        if (pub.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return pub.get();
     }
     public List<Pub> get(){
         return ((List<Pub>) pubCRUDRepository.findAll());
