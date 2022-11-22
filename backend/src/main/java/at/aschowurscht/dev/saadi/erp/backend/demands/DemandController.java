193a86a3dba1c52fd7ab7760f8f8657311409852
Henry <henry.rauchwerger@outlook.com>
2022-11-22 13:01:57 +0100

    increaseQuantity und decreaseQuantity Methoden in Demanservice aktualisiert

--- backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/demands/DemandController.java
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/demands/DemandController.java
@@ -1,24 +1,24 @@
 package at.aschowurscht.dev.saadi.erp.backend.demands;
 
-import at.aschowurscht.dev.saadi.erp.backend.products.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+
 public class DemandController {
     @Autowired
     DemandService demandService;
 
     @CrossOrigin
-    @PutMapping("api/demands/{proId}")
-    public void increaseQuantity(@PathVariable int proId){
-        demandService.highDemand(proId);
+    @PutMapping("api/demands/{proId}/{pubId}")
+    public void increaseQuantity(@PathVariable int proId, @PathVariable int pubId){
+        demandService.increaseQuantity(proId, pubId);
     }
 
     @CrossOrigin
-    @PutMapping("api/demands/{proId}/reduce")
-    public void reduceQuantity(@PathVariable int proId){
-        demandService.lowDemand(proId);
+    @PutMapping("api/demands/{proId}/{pubId}/reduce")
+    public void decreaseQuantity(@PathVariable int proId, @PathVariable int pubId){
+        demandService.decreaseQuantity(proId, pubId);
     }
 
 
--- backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/demands/DemandService.java
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/demands/DemandService.java
@@ -1,7 +1,5 @@
 package at.aschowurscht.dev.saadi.erp.backend.demands;
 
-
-import at.aschowurscht.dev.saadi.erp.backend.products.Product;
 import at.aschowurscht.dev.saadi.erp.backend.products.ProductCRUDRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,18 +12,17 @@ public class DemandService {
     @Autowired
     ProductCRUDRepository productCRUDRepository;
 
-    public void highDemand(int proId){
+    public void increaseQuantity(int proId, int pubId){
         for (Demand demands : demandCRUDRepository.findAll()){
-            if (demands.getProduct().getProId() == proId){
+            if (demands.getProduct().getProId() == proId && demands.getPub().getPubId() == pubId){
                 demands.setQuantity(demands.getQuantity()+1);
                 demandCRUDRepository.save(demands);
             }
         }
     }
-
-    public void lowDemand(int proId){
+    public void decreaseQuantity(int proId, int pubId){
         for (Demand demands : demandCRUDRepository.findAll()){
-            if (demands.getProduct().getProId() == proId){
+            if (demands.getProduct().getProId() == proId && demands.getPub().getPubId() == pubId ){
                 demands.setQuantity(demands.getQuantity()-1);
                 demandCRUDRepository.save(demands);
             }
