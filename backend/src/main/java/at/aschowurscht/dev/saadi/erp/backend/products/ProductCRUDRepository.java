fe30fe8fd4c48e1cffb38eeb5af9a2f1a8766b1c
Henry <henry.rauchwerger@outlook.com>
2022-11-23 12:29:15 +0100

    @Query findProductByVendor hinzugefügt

--- backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/products/ProductCRUDRepository.java
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/products/ProductCRUDRepository.java
@@ -1,10 +1,16 @@
 package at.aschowurscht.dev.saadi.erp.backend.products;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 
 
 @Repository
 public interface ProductCRUDRepository extends JpaRepository<Product,Integer> {
+    @Query("select p from Product p where p.vendor.venId = ?1")
+    List<Product> findProductByVendor(Integer venId);
 
 }
