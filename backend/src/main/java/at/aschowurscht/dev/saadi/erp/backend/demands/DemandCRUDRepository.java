10573020d7456cc015b3218e7651afb5843ccdb9
Henry <henry.rauchwerger@outlook.com>
2022-11-23 10:29:44 +0100

    @Query Statement finAmountOfQuantity implementiert

--- backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/demands/DemandCRUDRepository.java
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/demands/DemandCRUDRepository.java
@@ -1,13 +1,14 @@
 package at.aschowurscht.dev.saadi.erp.backend.demands;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
-public interface DemandCRUDRepository extends CrudRepository<Demand,DemandID> {
+public interface DemandCRUDRepository extends JpaRepository<Demand,DemandID> {
 
+    @Query("select sum(d.quantity) from Demand d where d.product.proId=?1")
+    int findAmountOfQuantity(Integer proId);
 
 }
