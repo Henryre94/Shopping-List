70a4f2338829e4f4212166646f18a0a4d80c8276
Henry <henry.rauchwerger@outlook.com>
2022-11-23 13:27:06 +0100

    Refractor von Methodennamen und @CrossOrigen annotration über die Klasse geschrieben

--- backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/demands/DemandController.java
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/demands/DemandController.java
@@ -1,4 +1,5 @@
 package at.aschowurscht.dev.saadi.erp.backend.demands;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,25 +8,24 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("api/demands")
+@CrossOrigin
 public class DemandController {
     @Autowired
     DemandService demandService;
 
-    @CrossOrigin
     @PutMapping("/{proId}/{pubId}")
-    public void increaseQuantity(@PathVariable int proId, @PathVariable int pubId){
+    public void increaseQuantity(@PathVariable int proId, @PathVariable int pubId) {
         demandService.increaseQuantity(proId, pubId);
     }
 
-    @CrossOrigin
     @PutMapping("/{proId}/{pubId}/reduce")
-    public void decreaseQuantity(@PathVariable int proId, @PathVariable int pubId){
+    public void decreaseQuantity(@PathVariable int proId, @PathVariable int pubId) {
         demandService.decreaseQuantity(proId, pubId);
     }
-    @CrossOrigin
+
     @GetMapping("/vendor/{venId}")
     @ResponseBody
-    public Set<DemandDto> getDemandsFromVendor(@PathVariable int venId){
+    public Set<DemandDto> getDemandsFromVendor(@PathVariable int venId) {
         return demandService.getAllDemandsFromVendor(venId);
     }
 }
