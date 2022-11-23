0f13cfe6cbaa7c972f8bbd51e65ab1138f4e1fe3
Henry <henry.rauchwerger@outlook.com>
2022-11-23 13:58:11 +0100

    unbenutzte import entfernt

--- backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/vendors/VendorCRUDRepository.java
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/vendors/VendorCRUDRepository.java
@@ -3,8 +3,6 @@ package at.aschowurscht.dev.saadi.erp.backend.vendors;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
 public interface VendorCRUDRepository extends CrudRepository<Vendor, Integer> {
 }
