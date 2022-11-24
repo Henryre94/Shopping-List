c052645fbfed02223af23b00d93210f2675fa99f
Henry <henry.rauchwerger@outlook.com>
2022-11-24 14:43:45 +0100

    Klasse ProductDto erstellt

---
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/products/ProductDto.java
@@ -0,0 +1,11 @@
+package at.aschowurscht.dev.saadi.erp.backend.products;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductDto {
+    String name;
+    String unit;
+}
