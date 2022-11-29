cee5413a6bdb8d5928239e6f2b1a3a894afe680c
Henry <henry.rauchwerger@outlook.com>
2022-11-29 13:16:28 +0100

    Neue DTOs

---
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/products/ProductNoIdDTO.java
@@ -0,0 +1,11 @@
+package at.aschowurscht.dev.saadi.erp.backend.products;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductNoIdDTO {
+    String name;
+    String unit;
+}
---
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/pubs/PubNoIdDTO.java
@@ -0,0 +1,12 @@
+package at.aschowurscht.dev.saadi.erp.backend.pubs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PubNoIdDTO {
+    String name;
+    int id;
+
+}
