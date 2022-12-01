bf1853089c4fd33bf416f1f08899a5ff2b6803b7
Ahmed El Seifi <ahmed.el-seifi@hotmail.com>
2022-12-01 09:21:26 +0100

    bugfixing, Warnung beim Löschen des Händlers bearbeitet.

--- frontend/src/views/VendorsAddView.vue
+++ frontend/src/views/VendorsAddView.vue
@@ -55,7 +55,7 @@
                     </v-dialog>
                     <v-dialog v-model="dialogDelete" max-width="500px">
                         <v-card>
-                            <v-card-title class="text-h5">Achtung! Der Händler wird gelöscht, alle Produkte des Händlers werden mitgelöscht!</v-card-title>
+                            <v-card-title class="text-h5">Achtung! Der Händler wird mit sämtlichen Produkten gelöscht !</v-card-title>
                             <v-card-actions>
                                 <v-spacer></v-spacer>
                                 <v-btn color="blue darken-1" text @click="closeDelete">Abbrechen</v-btn>
