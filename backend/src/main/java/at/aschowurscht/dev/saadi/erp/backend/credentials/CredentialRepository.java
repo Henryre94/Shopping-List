3d31fa0be4d07f0ab70b33bb9adf6516a698c3c7
Henry <henry.rauchwerger@outlook.com>
2022-12-06 14:44:41 +0100

    Unsatisfied dependency error corrected

--- backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/credentials/CredentialRepository.java
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/credentials/CredentialRepository.java
@@ -1,7 +1,9 @@
 package at.aschowurscht.dev.saadi.erp.backend.credentials;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CredentialRepository extends JpaRepository<Credentials, String> {
     Credentials findByUsername(String username);
 }
--- backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/security/TokenService.java
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/security/TokenService.java
@@ -8,6 +8,7 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
+
 import java.time.Instant;
 import java.util.stream.Collectors;
 
