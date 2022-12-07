4a2ad5946715e2cc8b0317080200524b41b244dc
Henry <henry.rauchwerger@outlook.com>
2022-12-07 11:55:52 +0100

    VendorDTO created and return for the putMethod from void to VendorDTO changed.

---
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/dtos/VendorDTO.java
@@ -0,0 +1,13 @@
+package at.aschowurscht.dev.saadi.erp.backend.dtos;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VendorDTO {
+    String name;
+    String address;
+    int venId;
+}
