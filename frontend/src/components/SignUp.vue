bff06e0d1659894dca9389be7243e70905e2491b
SilviaHammerer <silvia_hammerer@hotmail.com>
2022-12-08 22:32:14 +0100

    create SignUp component

---
+++ frontend/src/components/SignUp.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container>
+        <h1>Sign Up</h1>
+        <div class="register">
+            <input type="text" placeholder="Enter Name" />
+            <input type="text" placeholder="Enter Email" />
+            <input type="password" placeholder="Enter Password" />
+            <button> Sign Up</button>
+        </div>
+    </v-container>
+    
+</template>
+
+<script>
+export default {
+    name: 'SingUp'
+}
+</script>
+
+<style>
+.register input{
+    width: 300px;
+    height: 400px;
+    padding-left: 20px;
+    display: block;
+    margin-right: auto;
+    margin-left: auto;
+    margin-bottom: 30px;
+    border: 1px solid black;
+}
+</style>
