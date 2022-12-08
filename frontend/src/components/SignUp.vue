5de9e0af44765851032b16c0ee6d5fe3ac81b7d1
SilviaHammerer <silvia_hammerer@hotmail.com>
2022-12-08 22:50:41 +0100

    part of methods signUp

--- frontend/src/components/SignUp.vue
+++ frontend/src/components/SignUp.vue
@@ -2,10 +2,10 @@
     <v-container>
         <h1>Sign Up</h1>
         <div class="register">
-            <input type="text" placeholder="Enter Name" />
-            <input type="text" placeholder="Enter Email" />
-            <input type="password" placeholder="Enter Password" />
-            <button> Sign Up</button>
+            <input type="text" v-model="name" placeholder="Enter Name" />
+            <input type="text" v-model="email" placeholder="Enter Email" />
+            <input type="password" v-model="password" placeholder="Enter Password" />
+            <button @click="signUp"> Sign Up</button>
         </div>
     </v-container>
     
@@ -13,7 +13,19 @@
 
 <script>
 export default {
-    name: 'SingUp'
+    name: 'SingUp',
+    data() {
+        return {
+            name: '',
+            email:'',
+            password:''
+        }
+    },
+    methods: {
+        signUp() {
+            console.warn("signup", this.name, this.email, this.password);
+        }
+    }
 }
 </script>
 
