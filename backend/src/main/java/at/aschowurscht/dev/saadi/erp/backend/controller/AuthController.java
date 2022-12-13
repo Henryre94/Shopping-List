16a63aebb6e74c59f9589cefeb673bc00b111584
Henry <henry.rauchwerger@outlook.com>
2022-12-13 13:18:01 +0100

    AuthorizationDto as return value to the Get method from "/api/token"

--- backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/controller/AuthController.java
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/controller/AuthController.java
@@ -1,6 +1,7 @@
 package at.aschowurscht.dev.saadi.erp.backend.controller;
 
-import at.aschowurscht.dev.saadi.erp.backend.credentials.CredentialService;
+
+import at.aschowurscht.dev.saadi.erp.backend.dtos.AuthorizationDTO;
 import at.aschowurscht.dev.saadi.erp.backend.security.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -13,10 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final TokenService tokens;
-    private final CredentialService credentials;
 
     @GetMapping
-    public String generate(Authentication authentication) {
+    public AuthorizationDTO generate(Authentication authentication) {
         return tokens.generate(authentication);
     }
 
