4f625383a589231cabf959a2d4f225ac740ee56f
Silvia <silvia_hammerer@hotmail.com>
2022-12-13 13:28:02 +0100

    changed user to currentUser, commented out deleted

--- frontend/src/store/modules/LoginModule.js
+++ frontend/src/store/modules/LoginModule.js
@@ -1,28 +1,13 @@
 import axios from "axios";
 
-/*  Sebi 
-axios.interceptors.request.use(config => {
-  const jwt = localStorage.getItem('jwt');
-  if (jwt) {
-    // Falls der Token vom Server bereits mit "Bearer ..." kommt, dann kann man ihn hier auslassen
-    // config.headers.Authorization = 'Bearer ' + jwt;
-    config.headers.Authorization = jwt;
-  }
-  return config;
-})
-*/
-
-axios.defaults.headers.common["Authorization"] = "Bearer" + localStorage.getItem("token");
-
 export const LoginModule = {
     state: {
-        user: {},
-       
+        currentUser: null
     },
     getters: {},
     mutations: {
-        setUser(state, data) {
-            state.user = data;
+        setCurrentUser(state, user) {
+            state.currentUser = user;
         },
     },
     actions: {
@@ -35,19 +20,7 @@ export const LoginModule = {
                     },
                 });
                 localStorage.setItem("token", response.data.token);
-                context.commit('setCurrentUser', response.data.token);
-
-              //  .then((response) => {
-              //      console.log(response.data);
-              //      if (response.data.access_token) {
-                        //save token
-               //         localStorage.setItem("blog_token", response.data.access_token);
-                        // noch filtern wer was sehen darf
-                        //  this.$router.push('/')
-                       // window.location.replace("/");
-              //      }
-              //  });
-             // context.commit('setCurrentUser', response.data);
+                context.commit('setCurrentUser', response.data.credentials);
         },
     },
 };
