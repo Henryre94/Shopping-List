eab29b6e80741bf1a65f8eaaec5a49c4276578fd
Henry <henry.rauchwerger@outlook.com>
2022-12-13 12:43:11 +0100

    OneToOne mapping changed to credentials

--- backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/pubs/PubCRUDRepository.java
+++ backend/src/main/java/at/aschowurscht/dev/saadi/erp/backend/pubs/PubCRUDRepository.java
@@ -1,8 +1,10 @@
 package at.aschowurscht.dev.saadi.erp.backend.pubs;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PubCRUDRepository extends CrudRepository<Pub, Integer> {
+public interface PubCRUDRepository extends JpaRepository<Pub, Integer> {
+    Pub findPubByPubName(String pubName);
 }
