18aad0b9c5e4bd7a9df59125d698ed4d63513eba
SilviaHammerer <silvia_hammerer@hotmail.com>
2022-12-13 21:49:20 +0100

    Component Logout Button

---
+++ frontend/src/components/LogoutButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <v-btn @click="logout">Logout</v-btn>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LogoutButton",
+    methods: {
+        logout() {
+            console.log('geklickt')
+            this.$store.dispatch('logoutCurrentUser');
+         //   this.$router.push('/login')
+        },
+    },
+};
+</script>
