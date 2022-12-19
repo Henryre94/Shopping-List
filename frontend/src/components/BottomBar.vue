dd3aa7e8cd72e58aad340f7345778ab7ec2becd0
Ahmed El Seifi <ahmed.el-seifi@hotmail.com>
2022-12-19 13:36:45 +0100

    botoom navbar created

---
+++ frontend/src/components/BottomBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-bottom-navigation v-model="value" fixed >
+
+        <router-link style="text-decoration: none; color: inherit;" to="/">
+        <v-btn value="home">
+            <span>Startseite</span>
+
+            <v-icon x-large color="black">mdi-home-outline</v-icon>
+        </v-btn>
+        </router-link>
+
+<router-link style="text-decoration: none; color: inherit;" to="/haendler">
+        <v-btn value="vendor">
+            <span>Händler</span>
+
+            <v-icon x-large color="blue">mdi-store</v-icon>
+        </v-btn>
+</router-link>
+
+
+
+        <v-btn value="logout" @click="logout">
+            <span>logout</span>
+
+            <v-icon x-large color="red">mdi-logout</v-icon>
+        </v-btn>
+    </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+    data: () => ({ value: 'recent' }),
+    methods: {
+        logout() {
+            console.log('geklickt')
+            this.$store.dispatch('logoutCurrentUser');
+            //   this.$router.push('/login')
+        },
+    },
+}
+</script>
