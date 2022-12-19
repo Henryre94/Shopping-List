005536b7d4dea83030bee8fd7a51d69356639126
Silvia <silvia_hammerer@hotmail.com>
2022-12-19 10:05:49 +0100

    bugfixing with Ahmed

--- frontend/src/store/modules/DemandsModule.js
+++ frontend/src/store/modules/DemandsModule.js
@@ -56,9 +56,9 @@ export const DemandsModule = {
                 });
             store.commit('decreaseDemands', proId)
         },
-        async getVendorsDemand(store) {
-            
-            const response = await axios.get("/api/vendors/3/demands");
+        async getVendorsDemand(store, venId) {
+            // await axios.get("/api/demands/" + payload.demands.proId + "/pubs/" + payload.demands.pubId)
+            const response = await axios.get("/api/vendors/" + venId + "/demands");
             console.log(response)
             store.commit("getVendorsDemand", response.data)
         },
